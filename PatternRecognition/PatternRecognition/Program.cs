9e1f7de557233d2667809f03600ba5af2e5472d6
Alexander Nowak <howniceofyou@gmail.com>
2019-09-21 12:01:15 +0200

    Erstes Commit

---
+++ PatternRecognition/PatternRecognition/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PatternRecognition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
